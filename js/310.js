d0e9b8dff4e868a41f757c917e6f58a6851798d3
tianze889 <15915343658@139.com>
2026-04-02 02:24:11 +0800

    Create 310.js

---
+++ js/310.js
@@ -0,0 +1,22 @@
+var rule = {
+    title:'310鐩存挱',
+    host:'http://www.310.tv',
+    url:'/?s=0&t=1&a=fyclass&g=fypage',
+    searchUrl:'',
+    searchable:0,
+    quickSearch:0,
+    class_name:'鐑棬&瓒崇悆&绡悆',
+    class_url:'0&1&2',
+    headers:{
+        'User-Agent':'MOBILE_UA'
+    },
+    timeout:5000,
+    play_parse:false,
+    lazy:'',
+    limit:6,
+    double:false,
+    鎺ㄨ崘:'*',
+    涓€绾�:'.list_content a;.jiabifeng&&p:lt(5)&&Text;.feleimg img&&src;a&&t-nzf-o;a&&href',
+    浜岀骇:'*', 
+    鎼滅储:'',
+}
