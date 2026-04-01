91c6b14aaeda6dccc0278ed5aa8ce90e6a5e2160
tianze889 <15915343658@139.com>
2026-04-02 02:19:07 +0800

    Create a8yy.js

---
+++ js/a8yy.js
@@ -0,0 +1,20 @@
+var rule = {
+    title:'A8闊充箰',
+    host:'http://v.a8.com',
+    url:'/category/fyclass?p=fypage',
+    searchUrl:'/search?t=**',
+    searchable:2,
+    quickSearch:0,
+    headers:{
+        'User-Agent':'MOBILE_UA',
+    },
+    class_parse: '.categorys-inner li:gt(1):lt(7);a&&Text;a&&href;.*/(\\d+)',
+    timeout:5000,
+    limit:6,
+    play_parse:true,
+    lazy:'js:let html=request(input);let purl=jsp.pdfh(html,".video-js&&source&&src");input={jx:0,url:purl,parse:0}',
+    鎺ㄨ崘: '.v-list-body:eq(1)&&.v_item;*;*;*;*',
+    涓€绾�: '.list-div&&.v_item;img&&alt;img&&data-original;.v-time&&Text;a&&href',
+    浜岀骇:'*',
+    鎼滅储:'.v-list-body&&.v_item;*;*;*;*',
+}
